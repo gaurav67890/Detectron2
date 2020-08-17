00e44243491f569ea190439d5995a1c766822b4c
gaurav67890 <gauravsharma01091993@gmail.com>
2020-08-17 15:36:07 +0000

    cleaned dice coeff code

--- custom_test_net.py
+++ custom_test_net.py
@@ -34,67 +34,63 @@ register_coco_instances("scratch_test", {}, test_json, img_dir)
 
 cfg = get_cfg()
 cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TEST = ("scratch_val",)
+cfg.DATASETS.TEST = ("scratch_test",)
 cfg.DATALOADER.NUM_WORKERS = 4
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from mode$
 cfg.SOLVER.IMS_PER_BATCH = 1
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 12000 
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (scratch)
 cfg.TEST.EVAL_PERIOD = 500
 
 
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set a custom testing threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.4   # set a custom testing threshold for this model
 
 
 from detectron2.data import build_detection_test_loader
-evaluator = COCOEvaluator("scratch_test", cfg, False,output_dir="./output/")
 predictor = DefaultPredictor(cfg)
-val_loader = build_detection_test_loader(cfg, "scratch_test")
-#print(inference_on_dataset(trainer.model, val_loader, evaluator))
-# another equivalent way is to use trainer.test
-
 
 with open(test_json) as f:
         data = json.load(f)
 dice=0
 l=0
 for i in tqdm(range(len(data['images']))):
-    h=data['images'][i]['height']
-    w=data['images'][i]['width']
-    mask=np.zeros((h,w),dtype='uint8')
-    for j in range(len(data['annotations'])):
-        if data['annotations'][j]['image_id']==data['images'][i]['id']:
-            p1=data['annotations'][j]['segmentation'][0]
-            p1=[int(i) for i in p1]
-            p2=[]
-            for p in range(int(len(p1)/2)):
-                p2.append([p1[2*p],p1[2*p+1]])
-            fill_pts = np.array([p2], np.int32)
-            cv2.fillPoly(mask, fill_pts, 1)
+    try:
+        h=data['images'][i]['height']
+        w=data['images'][i]['width']
+        mask=np.zeros((h,w),dtype='uint8')
+        for j in range(len(data['annotations'])):
+            if data['annotations'][j]['image_id']==data['images'][i]['id']:
+                p1=data['annotations'][j]['segmentation'][0]
+                p1=[int(i) for i in p1]
+                p2=[]
+                for p in range(int(len(p1)/2)):
+                    p2.append([p1[2*p],p1[2*p+1]])
+                fill_pts = np.array([p2], np.int32)
+                cv2.fillPoly(mask, fill_pts, 1)
             
-    if np.unique(mask,return_counts=True)[1][1]/(w*h)>0.000:
-        l=l+1
-        #cv2.imwrite(data['images'][i]['file_name'],mask)
-        img=cv2.imread(img_dir+data['images'][i]['file_name'])
-        #cv2.imwrite('im/original'+str(i)+'.png',img)
-        #cv2.imwrite('im/mask'+str(i)+'.png',mask*255)
-        out = predictor(img)
-        pred = torch.sum(out['instances'].pred_masks,dim=0) > 0
-        pred = pred.cpu().detach().numpy()
-        pred=pred.astype(int)
-        #cv2.imwrite('im/pred'+str(i)+'.png',pred*255)
-        intersection = np.logical_and(mask, pred)
-        if len(np.unique(pred,return_counts=True)[1])>1:
-            ground=np.unique(mask,return_counts=True)[1][1]
-            pred_val=np.unique(pred,return_counts=True)[1][1]
-            dice_score = 2*np.sum(intersection) / (ground+pred_val)
+        if np.unique(mask,return_counts=True)[1][1]/(w*h)>0.000:
+            l=l+1
+            #cv2.imwrite(data['images'][i]['file_name'],mask)
+            img=cv2.imread(img_dir+data['images'][i]['file_name'])
+            #cv2.imwrite('im/original'+str(i)+'.png',img)
+            #cv2.imwrite('im/mask'+str(i)+'.png',mask*255)
+            out = predictor(img)
+            pred = torch.sum(out['instances'].pred_masks,dim=0) > 0
+            pred = pred.cpu().detach().numpy()
+            pred=pred.astype(int)
+            #cv2.imwrite('im/pred'+str(i)+'.png',pred*255)
+            intersection = np.logical_and(mask, pred)
+            if len(np.unique(pred,return_counts=True)[1])>1:
+                ground=np.unique(mask,return_counts=True)[1][1]
+                pred_val=np.unique(pred,return_counts=True)[1][1]
+                dice_score = 2*np.sum(intersection) / (ground+pred_val)
             #print(dice_score)
-        else:
-            dice_score=0
+            else:
+                dice_score=0
             #print(dice_score)
-        dice=dice+dice_score
+            dice=dice+dice_score
+    except Exception as e:
+        print(e)
 final_dice=dice/l
 print('Dice Coeff: '+str(final_dice))
 
