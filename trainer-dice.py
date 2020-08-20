f051635dccedb6d328477c7cbda12c34f763636b
gaurav67890 <gauravsharma01091993@gmail.com>
2020-08-19 19:47:27 -0500

    try except

--- trainer-dice.py
+++ trainer-dice.py
@@ -149,39 +149,42 @@ for md in model_list:
         dice=0
         l=0
         for i in tqdm(range(len(data['images']))):
-            h=data['images'][i]['height']
-            w=data['images'][i]['width']
-            mask=np.zeros((h,w),dtype='uint8')
-            for j in range(len(data['annotations'])):
-                if data['annotations'][j]['image_id']==data['images'][i]['id']:
-                    p1=data['annotations'][j]['segmentation'][0]
-                    p1=[int(i) for i in p1]
-                    p2=[]
-                    for p in range(int(len(p1)/2)):
-                        p2.append([p1[2*p],p1[2*p+1]])
-                    fill_pts = np.array([p2], np.int32)
-                    cv2.fillPoly(mask, fill_pts, 1)
-            if np.unique(mask,return_counts=True)[1][1]/(w*h)>0.000:
-                l=l+1
-                #cv2.imwrite(data['images'][i]['file_name'],mask)
-                img=cv2.imread(img_dir+data['images'][i]['file_name'])
-                #cv2.imwrite('im/original'+str(i)+'.png',img)
-                #cv2.imwrite('im/mask'+str(i)+'.png',mask*255)
-                out = predictor(img)
-                pred = torch.sum(out['instances'].pred_masks,dim=0) > 0
-                pred = pred.cpu().detach().numpy()
-                pred=pred.astype(int)
-                 #cv2.imwrite('im/pred'+str(i)+'.png',pred*255)
-                intersection = np.logical_and(mask, pred)
-                if len(np.unique(pred,return_counts=True)[1])>1:
-                    ground=np.unique(mask,return_counts=True)[1][1]
-                    pred_val=np.unique(pred,return_counts=True)[1][1]
-                    dice_score = 2*np.sum(intersection) / (ground+pred_val)
-            #print(dice_score)
-                else:
-                    dice_score=0
-            #print(dice_score)
-                dice=dice+dice_score
+            try:
+                h=data['images'][i]['height']
+                w=data['images'][i]['width']
+                mask=np.zeros((h,w),dtype='uint8')
+                for j in range(len(data['annotations'])):
+                    if data['annotations'][j]['image_id']==data['images'][i]['id']:
+                        p1=data['annotations'][j]['segmentation'][0]
+                        p1=[int(i) for i in p1]
+                        p2=[]
+                        for p in range(int(len(p1)/2)):
+                            p2.append([p1[2*p],p1[2*p+1]])
+                        fill_pts = np.array([p2], np.int32)
+                        cv2.fillPoly(mask, fill_pts, 1)
+                if np.unique(mask,return_counts=True)[1][1]/(w*h)>0.000:
+                    l=l+1
+                    #cv2.imwrite(data['images'][i]['file_name'],mask)
+                    img=cv2.imread(img_dir+data['images'][i]['file_name'])
+                    #cv2.imwrite('im/original'+str(i)+'.png',img)
+                    #cv2.imwrite('im/mask'+str(i)+'.png',mask*255)
+                    out = predictor(img)
+                    pred = torch.sum(out['instances'].pred_masks,dim=0) > 0
+                    pred = pred.cpu().detach().numpy()
+                    pred=pred.astype(int)
+                    #cv2.imwrite('im/pred'+str(i)+'.png',pred*255)
+                    intersection = np.logical_and(mask, pred)
+                    if len(np.unique(pred,return_counts=True)[1])>1:
+                        ground=np.unique(mask,return_counts=True)[1][1]
+                        pred_val=np.unique(pred,return_counts=True)[1][1]
+                        dice_score = 2*np.sum(intersection) / (ground+pred_val)
+                #print(dice_score)
+                    else:
+                        dice_score=0
+                #print(dice_score)
+                    dice=dice+dice_score
+            except Exception as e:
+                print(str(e))
         final_dice=dice/l
 
         print('Model name: '+md)
