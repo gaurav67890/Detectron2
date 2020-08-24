e20075c966406e6340190c2ba04211e2b0d9fe59
gaurav67890 <gauravsharma01091993@gmail.com>
2020-08-24 03:32:00 -0500

    p

--- trainer-main-gpu.py
+++ trainer-main-gpu.py
@@ -219,7 +219,8 @@ def setup(args):
     Create configs and perform basic setups.
     """
     cfg = get_cfg()
-    cfg.merge_from_file(args.config_file)
+    file_cfg='configs/COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml'
+    cfg.merge_from_file(file_cfg)
     cfg.merge_from_list(args.opts)
     #cfg.freeze()
     #default_setup(cfg, args)
