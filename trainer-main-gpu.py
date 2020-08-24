58223ecdfc141cc35a3ad95d822a673270c13a36
gaurav67890 <gauravsharma01091993@gmail.com>
2020-08-23 23:41:04 -0500

    removed freeze

--- trainer-main-gpu.py
+++ trainer-main-gpu.py
@@ -209,7 +209,7 @@ def setup(args):
     cfg = get_cfg()
     cfg.merge_from_file(args.config_file)
     cfg.merge_from_list(args.opts)
-    cfg.freeze()
+    #cfg.freeze()
     default_setup(cfg, args)
     return cfg
 
