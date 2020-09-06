fec973b22ca2a9d324f252ade98df32089a81a13
gaurav67890 <gauravsharma01091993@gmail.com>
2020-09-06 09:27:25 -0500

    removed evaluation

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -400,13 +400,14 @@ class DefaultTrainer(SimpleTrainer):
             OrderedDict of results, if evaluation is enabled. Otherwise None.
         """
         super().train(self.start_iter, self.max_iter)
+        '''
         if len(self.cfg.TEST.EXPECTED_RESULTS) and comm.is_main_process():
             assert hasattr(
                 self, "_last_eval_results"
             ), "No evaluation results obtained during training!"
             verify_results(self.cfg, self._last_eval_results)
             return self._last_eval_results
-
+        '''
     @classmethod
     def build_model(cls, cfg):
         """
