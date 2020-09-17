6b5ed5e35939b38a2ac87c534970ac355f699f97
gaurav67890 <gauravsharm101091993@gmail.com>
2020-09-17 16:16:43 +0000

    stat lenght increase

--- detectron2/evaluation/fast_eval_api.py
+++ detectron2/evaluation/fast_eval_api.py
@@ -155,7 +155,7 @@ class COCOeval_opt(COCOeval):
             print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
             return mean_s
         def _summarizeDets():
-            stats = np.zeros((12,))
+            stats = np.zeros((13,))
             stats[0] = _summarize(1)
             stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
             stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
